f9788fc2352145bae2733a0bb31a32f74d863730
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-02-28 15:58:28 -0500

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -3,23 +3,24 @@ DROP DATABASE IF EXISTS CSIT415;
 CREATE DATABASE CSIT415; 
 USE CSIT415; 
 
-# Step 2: Create a table (named administrator) with five fields.
-CREATE TABLE administrators( 
-   adminID           INT            NOT NULL   AUTO_INCREMENT, 
+# Step 2: Create a table (named accounts) with five fields.
+CREATE TABLE ACCOUNTS( 
+   accountID         INT            NOT NULL   AUTO_INCREMENT, 
    email             VARCHAR(255)   NOT NULL, 
-   password          VARCHAR(255)   NOT NULL, 
-   firstName         VARCHAR(255)   NOT NULL, 
-   lastName          VARCHAR(255)   NOT NULL, 
-  PRIMARY KEY (adminID) 
+   password          VARCHAR(50)    NOT NULL, 
+   firstName         VARCHAR(50)    NOT NULL, 
+   lastName          VARCHAR(50)    NOT NULL, 
+   account_type      VARCHAR(255)   NOT NULL,
+  PRIMARY KEY (accountID) 
 );
 
 # Step 3: Populate with three users. 
-INSERT INTO administrators (adminID, email, password, firstName, lastName) VALUES 
-(1, 'jj@gmail.com', 'sesame', 'John', 'Johnson'), 
-(2, 'kk@gmail.com', 'password', 'Karen', 'King'), 
-(3, 'sd@gmail.com', 'superman', 'Superman', 'Duper'); 
+INSERT INTO ACCOUNTS (accountID, email, password, firstName, lastName, account_type) VALUES 
+(1, 'jj@montclair.edu', 'sesame', 'John', 'Johnson', 'Teacher'), 
+(2, 'kk@montclair.edu', 'password', 'Karen', 'King', 'Student'), 
+(3, 'sd@montclair.edu', 'superman', 'Superman', 'Duper', 'Student'); 
 
-# Step 4: Create a user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
+# Step 4: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
 # Grant the user full privileges. 
 GRANT SELECT, INSERT, DELETE, UPDATE
 ON database_name.*
