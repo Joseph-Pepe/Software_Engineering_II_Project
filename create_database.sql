ee0bab5dca9e9c9117054d5e4ab502547030b286
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-02-28 15:49:32 -0500

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,30 @@
+# Step 1: Create and select the database (CSIT101).
+DROP DATABASE IF EXISTS CSIT101; 
+CREATE DATABASE CSIT101; 
+USE CSIT101; 
+
+# Step 2: Create a table (named administrator) with five fields.
+CREATE TABLE administrators( 
+   adminID           INT            NOT NULL   AUTO_INCREMENT, 
+   email             VARCHAR(255)   NOT NULL, 
+   password          VARCHAR(255)   NOT NULL, 
+   firstName         VARCHAR(255)   NOT NULL, 
+   lastName          VARCHAR(255)   NOT NULL, 
+  PRIMARY KEY (adminID) 
+);
+
+# Step 3: Populate with three users. 
+INSERT INTO administrators (adminID, email, password, firstName, lastName) VALUES 
+(1, 'jj@gmail.com', 'sesame', 'John', 'Johnson'), 
+(2, 'kk@gmail.com', 'password', 'Karen', 'King'), 
+(3, 'sd@gmail.com', 'superman', 'Superman', 'Duper'); 
+
+# Step 4: Create a user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
+# Grant the user full privileges. 
+GRANT SELECT, INSERT, DELETE, UPDATE
+ON database_name.*
+TO account_username@localhost
+IDENTIFIED BY 'account_user_password';
+
+
+
