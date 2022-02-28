e841affc291a757c74484cd6216592f5939f7a7b
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-02-28 15:30:06 -0500

    Create database_connection_error.php

---
+++ database_connection_error.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <main>
+	 <h1>Database Error:</h1>
+	 <p>There was an error connecting to the database.</p>
+	 <p>Error Message: <?php echo $error_message; ?></p>
+      </main>
+   </body>
+<html>
