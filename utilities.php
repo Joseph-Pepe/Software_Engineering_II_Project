8acfc2ed4424a9e7cad33fb764de12f5d2c0da99
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-03 11:09:06 -0500

    Create utilities.php

---
+++ utilities.php
@@ -0,0 +1,17 @@
+<?php
+// Make sure the page uses a secure connection:
+
+// HTTPS: Returns a non-empty value if the current request is using HTTPS.
+$https = filter_input(INPUT_SERVER, 'HTTPS');
+
+if(!$https){
+	// HTTP_HOST: Returns the host for another request.
+	$host = filter_input(INPUT_SERVER, 'HTTP_HOST');
+	
+	// REQUEST_URI: Returns the uniform resource identifier for the current request. 
+	$uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+	$url = 'https://' . $host . $uri; 
+	header("Location: " . $url);
+	exit();
+}
+?>
