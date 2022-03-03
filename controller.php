34e52561f6a337b539158421f53e10a4fdca793f
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-03 12:36:28 -0500

    Update controller.php

--- controller.php
+++ controller.php
@@ -2,5 +2,39 @@
 // Start session management and include necessary functions.
 session_start();
 require_once('model/database_connection.php');
+require_once('model/user_database.php');
 
+// Get the action to perform (e.g., login, view class roster)
+$action = filter_input(INPUT_POST, 'action');
+
+
+// If the user is not logged in, force the user to login.
+if(!isset($_SESSION['is_valid_user'])){
+   $action = 'login';
+}
+
+// Perform the specified action.
+switch($action){
+   case 'login':
+      $email = filter_input(INPUT_POST, 'email');
+      $password = filter_input(INPUT_POST, 'password');
+      if(is_valid_user_login($email, $password)){
+         $_SESSION['is_valid_user'] = true;
+         include('view/homepage.php');
+      }else{
+         $login_message = 'You must login to view this page.';
+         include('view/login_page.php');
+      }
+      break;
+   case 'logout':
+      // Clear all session data:
+      $_SESSION = array();
+      
+      // Clear up the session ID:
+      session_destroy();
+    
+      $login_message = 'You have been logged out.';
+      include('view/login_page.php');
+      break;
+}
 ?>
