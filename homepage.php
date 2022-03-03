4394bffb1b48f408f1c88f1b7635e22cae343eee
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-03 12:42:49 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,4 +1,9 @@
 <?php
+// Require a secure connection:
+require_once('utility/secure_connection.php');
+
+// Require a valid user:
+require_once('utility/valid_user.php');
 
 $user_email = filter_input(INPUT_POST, 'user_email');
 $user_password = filter_input(INPUT_POST, 'user_password');
