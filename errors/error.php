a4ee864296d322e4cf02c448a4f134298e493529
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-06 14:58:46 -0500

    Update error.php

--- errors/error.php
+++ errors/error.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+   <body>
+      <main>
+	       <h1>Error:</h1>
+	       <p><?php echo $error; ?></p>
+      </main>
+      <?php include '../view/footer.php'; ?>
+   </body>
+<html>
