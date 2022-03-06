368455cd07021f907fd7da40a87204739f5d4268
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-06 15:43:56 -0500

    Update valid_user_account.php

--- utility/valid_user_account.php
+++ utility/valid_user_account.php
@@ -1,8 +1,23 @@
 <?php
+require_once('model/database_connection.php');
+require_once('model/user_database.php');
 
-
-   // Make sure the user is logged in as a valid user account:
+/* Make sure the user is logged in as a valid user account:
    if(!isset($_SESSION['is_valid_user_account'])){
       header("Location: .") 
    }
+*/
+
+$email = '';
+$password = '';
+
+// 'PHP_AUTH_USER:' Is the username from the authentication dialog box or NULL if the box was not displayed.
+// 'PHP_AUTH_PW:'  Is the password from the authentication dialog box or NULL if the box was not displayed.
+if(!isset($_SERVER['PHP_AUTH_USER'])) && isset($_SERVER['PHP_AUTH_PW'])){
+   $email = $_SERVER['PHP_AUTH_USER'];
+   $password = $_SERVER['PHP_AUTH_PW'];
+}
+
+
+
 ?>
