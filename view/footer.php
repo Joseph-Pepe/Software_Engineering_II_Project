75956bc1808ee627c00f366a308413d64d0825f2
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 15:21:53 -0400

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,8 @@
 <footer>
+      <?php if()){ ?>
+	    
+	 <?php } ?>
+
       <h5 class = "copyright">
          &copy; <?php echo date("Y"); ?> by Company_Name_Here, Inc. All rights reserved.
       </h5>
