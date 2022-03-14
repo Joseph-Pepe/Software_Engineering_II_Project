992828b4712dbc25cfa5abfae45744cbd884d7fb
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 15:34:32 -0400

    Update homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -8,6 +8,12 @@ require_once('utility/valid_user_account.php');
 
 <!DOCTYPE html>
 <html>
+   <head>
+     <meta charset = "utf-8">
+     <title>Homepage</title>
+     <link rel = "stylesheet" type = "text/css" href = "styles.css"/>
+   </head>
+
 
    <?php if($user_role == "instructor"){ ?>
 	    
