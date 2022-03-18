938be0adbda4d01592e0b21bf85c332d54e715eb
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 19:46:46 -0400

    Update database_connection.php

--- model/database_connection.php
+++ model/database_connection.php
@@ -2,11 +2,12 @@
 $data_source_name = 'mysql:host=localhost;dbname=database_name';
 $username = 'account_username';
 $password = 'account_user_password';
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 // Handles a PHP data object (PDO) exception.
 try{
    // Create a database object:
-   $database = new PDO($data_source_name, $username, $password); 
+   $database = new PDO($data_source_name, $username, $password, $options); 
    echo '<p>You are connected to the database!</p>';
 }// Catches PDO exception:
 catch(PDOException $database_exception){
@@ -15,9 +16,11 @@ catch(PDOException $database_exception){
    include('../errors/database_connection_error.php');
    exit();
    //echo "<p>An error occurred while connecting to the database: $error_message</p>";
-}// Catches any type of exception:
+}
+/* Catches any type of exception:
 catch(Exception $exception){
    // Executes the object's method.
    //$error_message = $exception->getMessage();
    //echo "<p>Any Error Message: $error_message</p>";
 }
+*/
