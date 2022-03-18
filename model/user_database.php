bdef6799919a8bd3e04c0a4ab12eab2ac4779350
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 18:32:17 -0400

    Update user_database.php

--- model/user_database.php
+++ model/user_database.php
@@ -4,12 +4,15 @@ function add_user($email, $password){
   
    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
   
-   $add_user_database_query = 'INSERT INTO ACCOUNTS (email, password) VALUES (:email, :password)';
+   $add_user_database_query = 'INSERT INTO ACCOUNTS (first_name, last_name, email, password, account_type) VALUES (:first_name, :last_name, :email, :password, :account_type)';
    $add_user = $db->prepare($add_user_database_query);
  
    // Method bindValue(parameter, value):
+   $add_user->bindValue(':first_name', $first_name);
+   $add_user->bindValue(':last_name', $last_name);
    $add_user->bindValue(':email', $email);
    $add_user->bindValue(':password', $hashed_password);
+   $add_user->bindValue(':account_type', $account_type);
 
    // Execute SQL statement:
    $add_user->execute();
