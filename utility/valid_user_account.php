f3e0cafb756656e2194400ecffd0a1ecfe31d304
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 19:24:05 -0400

    Update valid_user_account.php

--- utility/valid_user_account.php
+++ utility/valid_user_account.php
@@ -2,11 +2,11 @@
 require_once('model/database_connection.php');
 require_once('model/user_database.php');
 
-/* Make sure the user is logged in as a valid user account:
-   if(!isset($_SESSION['is_valid_user_account'])){
-      header("Location: .") 
-   }
-*/
+// Make sure the user is logged in as a valid user account:
+if(!isset($_SESSION['is_valid_user_account'])){
+   header("Location: ."); 
+}
+
 
 $email = '';
 $password = '';
