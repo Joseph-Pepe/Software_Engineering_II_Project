424ba0e07f4ca5f4ed3e4e8e72be215f0e6968d8
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 19:57:36 -0400

    Update login_page.php

--- view/login_page.php
+++ view/login_page.php
@@ -20,32 +20,27 @@ require_once('utility/secure_connection.php');
     <form method = "post" id = "login_form" class = "aligned" action = ".">
 	 <input name = "action" type = "hidden" value = "login"/>
 	 <!-- [Form]: Stores the form data in a $_POST array (can retrieve the values from the superglobal variables). -->
-	 <div id = "data">
-	    <!--
-	    <label>First Name:</label>
-            <input type = "text" name = "first_name" required/>
+	 <!--
+	 <label>First Name:</label>
+         <input type = "text" name = "first_name" required/>
 
-	    <label>Last Name:</label>
-	    <input type = "text" name = "last_name" required/>
-            -->
-	    <label>Email:</label>
-	    <input type = "text" name = "email" required/>
+	 <label>Last Name:</label>
+	 <input type = "text" name = "last_name" required/>
+         -->
+	 <label>Email:</label>
+	 <input type = "text" name = "email" required/>
 
-	    <label>Password:</label>
-	    <input type = "text" name = "password" required/>
-	 </div>
+	 <label>Password:</label>
+	 <input type = "text" name = "password" required/>
 	
 	 <select name = "account_type">
 	    <option value = "student">Student</option>
 	    <option value = "instructor">Instructor</option>
 	 </select>
 	
-	
-	 <div id = "buttons" align = "center">
-	    <label>&nbsp;</label>
-	    <input type = "submit" value = "Login"/>
-	    <input type = "reset" value = "Clear"/>
-	 </div>
+	 <label>&nbsp;</label>
+	 <input type = "submit" value = "Login"/>
+	 <input type = "reset" value = "Clear"/>
 	
 	 <?php if(!empty($login_message)){ ?>
 	    <b class = "error" align = "center" style = "color: red;">Error:</b><?php echo htmlspecialchars($login_message); ?>
