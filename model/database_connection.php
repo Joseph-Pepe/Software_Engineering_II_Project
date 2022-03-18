3f2058fd08c9bc2b449225464ff569e6175593f2
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 18:07:22 -0400

    Update database_connection.php

--- model/database_connection.php
+++ model/database_connection.php
@@ -1,23 +1,23 @@
 // Information to connect to a database:
-$data_source_name = 'mysql:host=host_address;dbname=database_name';
+$data_source_name = 'mysql:host=localhost;dbname=database_name';
 $username = 'account_username';
 $password = 'account_user_password';
 
 // Handles a PHP data object (PDO) exception.
 try{
-	// Create a database object:
-	$database = new PDO($data_source_name, $username, $password); 
-	echo '<p>You are connected to the database!</p>';
+   // Create a database object:
+   $database = new PDO($data_source_name, $username, $password); 
+   echo '<p>You are connected to the database!</p>';
 }// Catches PDO exception:
 catch(PDOException $database_exception){
-	// Executes the object's method.
-	$error_message = $database_exception->getMessage();
-	include('../errors/database_connection_error.php');
-	exit();
-	//echo "<p>An error occurred while connecting to the database: $error_message</p>";
+   // Executes the object's method.
+   $error_message = $database_exception->getMessage();
+   include('../errors/database_connection_error.php');
+   exit();
+   //echo "<p>An error occurred while connecting to the database: $error_message</p>";
 }// Catches any type of exception:
 catch(Exception $exception){
-	// Executes the object's method.
-	//$error_message = $exception->getMessage();
-	//echo "<p>Any Error Message: $error_message</p>";
+   // Executes the object's method.
+   //$error_message = $exception->getMessage();
+   //echo "<p>Any Error Message: $error_message</p>";
 }
