d910930af81fb9547b769033cec5ce61c6e3ca0b
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-18 20:04:05 -0400

    Update login_page.php

--- view/login_page.php
+++ view/login_page.php
@@ -16,36 +16,35 @@ require_once('utility/secure_connection.php');
        <h1>Login</h1>
        <hr/>
     </header>
-    
-    <form method = "post" id = "login_form" class = "aligned" action = ".">
-	 <input name = "action" type = "hidden" value = "login"/>
-	 <!-- [Form]: Stores the form data in a $_POST array (can retrieve the values from the superglobal variables). -->
-	 <!--
-	 <label>First Name:</label>
-         <input type = "text" name = "first_name" required/>
+    <main>
+       <form method = "post" id = "login_form" class = "aligned" action = ".">
+          <input name = "action" type = "hidden" value = "login"/>
+	  <!-- [Form]: Stores the form data in a $_POST array (can retrieve the values from the superglobal variables). -->
+	   <!--
+	  <label>First Name:</label>
+          <input type = "text" name = "first_name" required/>
 
-	 <label>Last Name:</label>
-	 <input type = "text" name = "last_name" required/>
-         -->
-	 <label>Email:</label>
-	 <input type = "text" name = "email" required/>
+	  <label>Last Name:</label>
+	  <input type = "text" name = "last_name" required/>
+          -->
+	  <label>Email:</label>
+	  <input type = "text" name = "email" required/>
 
-	 <label>Password:</label>
-	 <input type = "text" name = "password" required/>
+	  <label>Password:</label>
+	  <input type = "text" name = "password" required/>
 	
-	 <select name = "account_type">
-	    <option value = "student">Student</option>
-	    <option value = "instructor">Instructor</option>
-	 </select>
+	  <select name = "account_type">
+	     <option value = "student">Student</option>
+	     <option value = "instructor">Instructor</option>
+	  </select>
 	
-	 <label>&nbsp;</label>
-	 <input type = "submit" value = "Login"/>
-	 <input type = "reset" value = "Clear"/>
-	
-	 <?php if(!empty($login_message)){ ?>
-	    <b class = "error" align = "center" style = "color: red;">Error:</b><?php echo htmlspecialchars($login_message); ?>
-	 <?php } ?>
-    </form>
-    <?php include 'view/footer'; ?>
+	  <label>&nbsp;</label>
+	  <input type = "submit" value = "Login"/>
+	  <input type = "reset" value = "Clear"/>
+	 
+	  <b class = "error" align = "center" style = "color: red;"><?php echo $login_message; ?></b>
+       </form>
+       <?php include 'view/footer'; ?>
+    <main>
  </body>	
 </html>
