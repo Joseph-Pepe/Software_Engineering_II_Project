e863360c52471537f9ad15e64cb818022808c8dd
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 14:18:47 -0400

    Add files via upload

---
+++ create_database_2.sql
@@ -0,0 +1,31 @@
+-- Step 1: Create and select the database (CSIT415).
+DROP DATABASE IF EXISTS CSIT415; 
+CREATE DATABASE CSIT415; 
+USE CSIT415; 
+
+-- Step 2: Create a table (named accounts) with multiple fields.
+CREATE TABLE ACCOUNTS( 
+   account_id        INT            NOT NULL   AUTO_INCREMENT, 
+   email_address     VARCHAR(255)   NOT NULL, 
+   password          VARCHAR(50)    NOT NULL,
+   first_name        VARCHAR(50)    NOT NULL, 
+   last_name         VARCHAR(50)    NOT NULL, 
+   account_type      VARCHAR(50)    NOT NULL,
+   PRIMARY KEY (account_id) 
+);
+
+-- Step 3: Populate with three users. 
+INSERT INTO ACCOUNTS (account_id, email_address, password, first_name, last_name,  account_type) VALUES 
+(1, 'jj@montclair.edu', 'sesame', 'John', 'Johnson', 'Teacher'), 
+(2, 'kk@montclair.edu', 'password', 'Karen', 'King',  'Student'), 
+(3, 'sd@montclair.edu', 'Duper', 'superman', 'Superman', 'Student'); 
+
+
+-- Step 4: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
+-- Creates a user and grants them full privileges. 
+GRANT SELECT, INSERT, DELETE, UPDATE
+ON *
+TO account_username@localhost
+IDENTIFIED BY 'account_user_password';
+
+
