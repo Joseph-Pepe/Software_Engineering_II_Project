d0ee268a291476ee4adadbf7f02ecfecd1c0925e
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 16:47:20 -0400

    Update database_connection.php

--- model/database_connection.php
+++ model/database_connection.php
@@ -3,7 +3,7 @@
 $data_source_name = 'mysql:host=localhost;dbname=csit415';
 $username = 'canvas';
 $password = 'software';
-$options = array(PDO::ATTR_MODE => PDO::ERRMODE_EXCEPTION);
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 // [Try/Catch]: Handles a PHP data object (PDO) exception:
 try{
