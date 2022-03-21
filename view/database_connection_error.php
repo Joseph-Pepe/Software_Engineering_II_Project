424653169b5dbd8ab1291afaaa31c3fd38ab70b5
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 15:14:08 -0400

    Create database_connection_error.php

---
+++ view/database_connection_error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <main>
+	 <h1>Database Error:</h1>
+	 <p>There was an error connecting to the database.</p>
+	 <p>Error Message: <?php echo $error_message; ?></p>
+      </main>
+      <?php include '../view/footer.php'; ?>
+   </body>
+<html>
