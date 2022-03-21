007e1d46a0037ece16fc768d8e705fdec90d0c2c
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 17:22:58 -0400

    Update homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -1,6 +1,6 @@
 <?php
 // Require a secure connection:
-require_once('utility/secure_connection.php');
+// require_once('utility/secure_connection.php');
 
 // Require a valid user:
 require_once('utility/valid_user_account.php');
