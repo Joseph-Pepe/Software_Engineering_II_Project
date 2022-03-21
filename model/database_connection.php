2e17b71cb85e08207b8dbebc9d7498121fd0e094
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 16:37:51 -0400

    Update database_connection.php

--- model/database_connection.php
+++ model/database_connection.php
@@ -8,7 +8,6 @@ $options = array(PDO::ATTR_MODE => PDO::ERRMODE_EXCEPTION);
 // [Try/Catch]: Handles a PHP data object (PDO) exception:
 try{
    $database = new PDO($data_source_name, $username, $password, $options);
-   echo '<p>You are connected to the database (success)!</p>';
 }catch(PDOException $exception_object){
    $error_message = $exception_object->getMessage();
    include('../view/database_connection_error.php');
