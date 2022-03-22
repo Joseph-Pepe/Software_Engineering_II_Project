eda6c6b02334b51b1f0665a07fb92ded752494c4
IIkevinII <60191548+IIkevinII@users.noreply.github.com>
2022-03-22 13:19:55 -0400

    Delete loginSignUp.js

--- loginSignUp.js
+++
@@ -1,9 +0,0 @@
-$('.showLogin').on('click', function() {
-    $('.signUpForm').hide();
-    $('.loginForm').show();
-});
-
-$('.showCreate').on('click', function() {
-    $('.loginForm').hide();
-    $('.signUpForm').show();
-});
