29eeb4e78f8010eda71b7715027163f333422ed7
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-21 20:27:04 -0400

    Update process_add.php

--- view/process_add.php
+++ view/process_add.php
@@ -1,3 +1,11 @@
 <?php
-
+// Store user data in local variables
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+$first_name = filter_input(INPUT_POST, 'first_name');
+$last_name = filter_input(INPUT_POST, 'last_name');
+$account_type = filter_input(INPUT_POST, 'account_type');
+        
+// Add the customer data to the database
+$user_id = add_user($email, $first_name, $last_name, $password, $account_type);
 ?>
