3ed0183697765f4d69669fc19c4279ae0d62519d
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 12:50:29 -0400

    Update main.php

--- utility/main.php
+++ utility/main.php
@@ -16,23 +16,23 @@ require_once('model/database.php');
 
 // Define some common functions
 function display_db_error($error_message) {
-    global $app_path;
-    include 'errors/db_error.php';
-    exit;
+   global $app_path;
+   include 'errors/db_error.php';
+   exit;
 }
 
 function display_error($error_message) {
-    global $app_path;
-    include 'errors/error.php';
-    exit;
+   global $app_path;
+   include 'errors/error.php';
+   exit;
 }
 
 function redirect($url) {
-    session_write_close();
-    header("Location: " . $url);
-    exit;
+   session_write_close();
+   header("Location: " . $url);
+   exit;
 }
 
-// Start session to store user and cart data
+// Start session to store user data
 session_start();
 ?>
