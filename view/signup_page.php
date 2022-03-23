e3571cfca2ff2f77a43fab842a135bf46de03bd3
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 13:40:30 -0400

    Update signup_page.php

--- view/signup_page.php
+++ view/signup_page.php
@@ -8,61 +8,46 @@ if (!isset($password_message)) { $password_message = ''; }
 <html>
  <head>
      <meta charset = "utf-8">
-     <title>Login</title>
+     <title>Signup</title>
      <link rel = "stylesheet" type = "text/css" href = "styles.css"/>
  </head>
 
  <body>
     <header>
-       <h1>Login</h1>
+       <h1>Account Signup</h1>
        <hr/>
     </header>
     <main>
-       <form method = "post" id = "login_form" class = "aligned" action = ".">
-          <input name = "action" type = "hidden" value = "login"/>
-	  
-	  <label>Email:</label>
-	  <input type = "text" name = "email" required/>
-
-	  <label>Password:</label>
-	  <input type = "text" name = "password" required/>
-	
-	  <label>&nbsp;</label>
-	  <input type = "submit" value = "Login"/>
-	  <input type = "reset" value = "Clear"/>
-       </form>
-       <b class = "error" align = "center" style = "color: red;"><?php echo $login_message; ?></b>
-    </main>
-    <header>
-       <h1>Create Account (Signup)</h1>
-       <hr/>
-    </header> 
-    <main>
-       <form method = "post" id = "signup_form" class = "aligned" action = "view/process_add.php">
+       <form method = "post" id = "signup_form" class = "aligned" action = ".">
           <input name = "action" type = "hidden" value = "signup"/>
 	       
-	  <label>First Name:</label>
-          <input type = "text" name = "first_name" required/>
+	  <label>E-Mail:</label>
+          <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>" size="30">
+          <?php echo $fields->getField('email')->getHTML(); ?><br>
 
-	  <label>Last Name:</label>
-	  <input type = "text" name = "last_name" required/>
-          
-	  <label>Email:</label>
-	  <input type = "text" name = "email" required/>
+          <label>Password:</label>
+          <input type="password" name="password_1" size="30">
+          <?php echo $fields->getField('password_1')->getHTML(); ?>
+          <span class="error"><?php echo htmlspecialchars($password_message); ?></span><br>
 
-	  <label>Password:</label>
-	  <input type = "password" name = "password" required/>
-	       
-	  <select name = "account_type">
-	     <option value = "student">Student</option>
-	     <option value = "instructor">Instructor</option>
-	  </select>
+          <label>Retype Password:</label>
+          <input type="password" name="password_2" size="30">
+          <?php echo $fields->getField('password_2')->getHTML(); ?><br>
+
+          <label>First Name:</label>
+          <input type="text" name="first_name" value="<?php echo htmlspecialchars($first_name); ?>" size="30">
+          <?php echo $fields->getField('first_name')->getHTML(); ?><br>
+
+          <label>Last Name:</label>
+          <input type="text" name="last_name" value="<?php echo htmlspecialchars($last_name); ?>" size="30">
+          <?php echo $fields->getField('last_name')->getHTML(); ?><br>
 	
 	  <label>&nbsp;</label>
 	  <input type = "submit" value = "Signup"/>
 	  <input type = "reset" value = "Clear"/>
        </form>
-       <?php include 'footer.php'; ?>
+       <!-- <b class = "error" align = "center" style = "color: red;"><?php echo $login_message; ?></b> -->
     </main>
+    <?php include 'footer.php'; ?>
  </body>	
 </html>
