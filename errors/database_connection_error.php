aec9586c1ca324ecb8059c8621c101857fa42a72
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 13:26:39 -0400

    Update database_connection_error.php

--- errors/database_connection_error.php
+++ errors/database_connection_error.php
@@ -2,7 +2,7 @@
 <html>
    <body>
       <main>
-	 <h1>Database Error:</h1>
+	 <h1>Database Connection Error:</h1>
 	 <p>There was an error connecting to the database.</p>
 	 <p>Error Message: <?php echo $error_message; ?></p>
       </main>
