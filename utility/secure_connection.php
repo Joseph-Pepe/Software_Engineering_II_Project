60bc5cfed0db1016777f992d83587f206289ba0a
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 12:54:14 -0400

    Update secure_connection.php

--- utility/secure_connection.php
+++ utility/secure_connection.php
@@ -6,10 +6,10 @@ $https = filter_input(INPUT_SERVER, 'HTTPS');
 
 if(!$https){
    // HTTP_HOST: Returns the host for another request.
-   $host = filter_input(INPUT_SERVER, 'HTTP_HOST');
+   $host = filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING);
 	
    // REQUEST_URI: Returns the uniform resource identifier for the current request. 
-   $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
+   $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', , FILTER_SANITIZE_STRING);
    $url = 'https://' . $host . $uri; 
    header("Location: " . $url);
    exit();
