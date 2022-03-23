6ac80e0b53512a12e7793d084966bf53f4f6f7d1
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 13:48:47 -0400

    Update account_login_signup.php

--- view/account_login_signup.php
+++ view/account_login_signup.php
@@ -32,36 +32,6 @@
        </form>
        <b class = "error" align = "center" style = "color: red;"><?php echo $login_message; ?></b>
     </main>
-    <header>
-       <h1>Create Account (Signup)</h1>
-       <hr/>
-    </header> 
-    <main>
-       <form method = "post" id = "signup_form" class = "aligned" action = "view/process_add.php">
-          <input name = "action" type = "hidden" value = "signup"/>
-	       
-	  <label>First Name:</label>
-          <input type = "text" name = "first_name" required/>
-
-	  <label>Last Name:</label>
-	  <input type = "text" name = "last_name" required/>
-          
-	  <label>Email:</label>
-	  <input type = "text" name = "email" required/>
-
-	  <label>Password:</label>
-	  <input type = "password" name = "password" required/>
-	       
-	  <select name = "account_type">
-	     <option value = "student">Student</option>
-	     <option value = "instructor">Instructor</option>
-	  </select>
-	
-	  <label>&nbsp;</label>
-	  <input type = "submit" value = "Signup"/>
-	  <input type = "reset" value = "Clear"/>
-       </form>
-       <?php include 'footer.php'; ?>
-    </main>
+    <?php include 'footer.php'; ?>
  </body>	
 </html>
