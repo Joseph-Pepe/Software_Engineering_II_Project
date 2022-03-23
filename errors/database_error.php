fd08961cdacec2521044d913e8447ee117041f43
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-23 12:03:53 -0400

    Update database_error.php

--- errors/database_error.php
+++ errors/database_error.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+   <body>
+      <main>
+         <h1>Database Error:</h1>
+	       <p><?php echo $error_message; ?></p>
+      </main>
+      <?php include 'footer.php'; ?>
+   </body>
+<html>
