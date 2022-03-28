9c65b4eb4961595f2838aba27f230bb84cb8efe9
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-28 19:40:21 -0400

    Update valid_user_account.php

--- utility/valid_user_account.php
+++ utility/valid_user_account.php
@@ -6,23 +6,4 @@ require_once('model/user_database.php');
 if(!isset($_SESSION['is_valid_user_account'])){
    header("Location: ."); 
 }
-
-/*
-$email = '';
-$password = '';
-
-// 'PHP_AUTH_USER:' Is the username from the authentication dialog box or NULL if the box was not displayed.
-// 'PHP_AUTH_PW:'  Is the password from the authentication dialog box or NULL if the box was not displayed.
-if(!isset($_SERVER['PHP_AUTH_USER'])) && isset($_SERVER['PHP_AUTH_PW'])){
-   $email = $_SERVER['PHP_AUTH_USER'];
-   $password = $_SERVER['PHP_AUTH_PW'];
-}
-
-if(!is_valid_user_login($email, $password)){
-   header('WWW-Authenticate: Basic realm = "Admin"');
-   header('HTTP/1.0 401 Unauthorized');
-   include('unauthorized.php');
-   exit();
-}
-*/
 ?>
