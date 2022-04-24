01660bcec25b7899b5d30bbdb22174ee2ca99110
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-04-24 09:46:26 -0400

    Update side_bar.php

--- view/side_bar.php
+++ view/side_bar.php
@@ -6,11 +6,10 @@
         </li>
             <?php
             // Check if user is logged in and display appropriate account links
-            $account_url = $app_path . 'account';
+            // $account_url = $app_path . 'account';
             $logout_url = $account_url . '?action=logout';
             if (isset($_SESSION['user'])) :
             ?>
-                <li><a href="<?php echo $account_url; ?>">My Account</a></li>
                 <li><a href="<?php echo $logout_url; ?>">Logout</a>
             <?php else: ?>
                 <li><a href="<?php echo $account_url; ?>">Login/Register</a></li>
