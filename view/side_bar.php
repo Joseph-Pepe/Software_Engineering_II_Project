e1b21851b17b2acfff469b1eb5c0f078df6582df
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-04-24 09:45:32 -0400

    Update side_bar.php

--- view/side_bar.php
+++ view/side_bar.php
@@ -19,25 +19,4 @@
             <a href="<?php echo $app_path; ?>">Home</a>
         </li>
     </ul>
-        
-    <h2>Categories</h2>
-    <ul>
-        <!-- display links for all categories -->
-        <?php
-            require_once('model/database.php');
-            require_once('model/category_db.php');
-            
-            $categories = get_categories();
-            foreach($categories as $category) :
-                $name = $category['categoryName'];
-                $id = $category['categoryID'];
-                $url = $app_path . 'catalog?category_id=' . $id;
-        ?>
-        <li>
-            <a href="<?php echo $url; ?>">
-               <?php echo htmlspecialchars($name); ?>
-            </a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
 </aside>
