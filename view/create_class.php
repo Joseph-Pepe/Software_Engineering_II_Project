752cfd702ff6cfede92a9cf332456c48601cc5f3
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-04-28 19:31:02 -0400

    Update create_class.php

--- view/create_class.php
+++ view/create_class.php
@@ -15,10 +15,25 @@
           <input type="text" name="class_name" size="30">
 
           <label>Term:</label>
-          <input type="password" name="password" size="30">
+          <input type="text" name="term" size="30">
+	       
+	  <label>Start Time:</label>
+          <input type="text" name="start_time" size="30">
+	       
+	  <label>End Time:</label>
+          <input type="text" name="end_time" size="30">
+	       
+	  <label>Start Date:</label>
+          <input type="text" name="start_date" size="30">
+	       
+	  <label>End Date:</label>
+          <input type="text" name="end_date" size="30">
+	       
+	  <label>Day:</label>
+          <input type="text" name="day" size="30">
 	   
           <input type="submit" value="Create Course">
-	        <input type = "reset" value = "Clear"/>
+	  <input type = "reset" value = "Clear"/>
        </form>
     </main>
     <?php include 'footer.php'; ?>
