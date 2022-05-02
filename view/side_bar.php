db56b01b11c5e9580000657239ac417504fc03a4
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-02 19:43:01 -0400

    Update side_bar.php

--- view/side_bar.php
+++ view/side_bar.php
@@ -1,25 +1,19 @@
 <aside>
-    <h2>Actions (Links)</h2>
-    <ul>
-        
-        <!--
-        <li>
-            <a href="<?php echo $app_path . 'classes'; ?>">View Classes</a>
-        </li>
-        
-        <?php
-            // Logout Account:
-            $logout_url = $app_path . '?action=logout';
+     <h2>Actions (Links)</h2>
+     <ul>
+         <li>
+             <a href="<?php echo $app_path; ?>">Home</a>
+         </li>
 
-            // Home Page:
-            $home_page_url = $app_path . '?action=view_homepage';
-        ?>
-        <li>
-            <a href="<?php echo $home_page_url; ?>">Home</a>
-        </li>
-
-        <li>
-            <a href="<?php echo $logout_url; ?>">Logout</a>
-        </li>
+         <li>
+             <a href="<?php echo $app_path . 'classes'; ?>">View Classes</a>
+         </li>
+         <?php
+             // Logout Account:
+             $logout_url = $app_path . '?action=logout';
+         ?>
+         <li>
+             <a href="<?php echo $logout_url; ?>">Logout</a>
+         </li>
     </ul>
 </aside>
