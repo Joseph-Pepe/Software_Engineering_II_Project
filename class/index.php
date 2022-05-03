f679c0a30c700953d6f7303825b45f94a6d1a29b
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-02 20:39:10 -0400

    Update index.php

--- class/index.php
+++ class/index.php
@@ -78,55 +78,5 @@ switch ($action) {
             include('product_view.php');
         }
         break;
-    case 'update_product':
-        $product_id = filter_input(INPUT_POST, 'product_id', 
-                FILTER_VALIDATE_INT);
-        $category_id = filter_input(INPUT_POST, 'category_id', 
-                FILTER_VALIDATE_INT);
-        $code = filter_input(INPUT_POST, 'code');
-        $name = filter_input(INPUT_POST, 'name');
-        $description = filter_input(INPUT_POST, 'description');
-        $price = filter_input(INPUT_POST, 'price', 
-                FILTER_VALIDATE_FLOAT);
-        $discount_percent = filter_input(INPUT_POST, 'discount_percent', 
-                FILTER_VALIDATE_FLOAT);
-
-        // Validate inputs
-        if (empty($code) || empty($name) || empty($description) ||
-            $price === false || $discount_percent === false ) {
-            $error = 'Invalid product data.
-                      Check all fields and try again.';
-            include('../../errors/error.php');
-        } else {
-            $categories = get_categories();
-            update_product($product_id, $code, $name, $description,
-                           $price, $discount_percent, $category_id);
-            $product = get_product($product_id);
-            include('product_view.php');
-        }
-        break;
-    case 'upload_image':
-        $product_id = filter_input(INPUT_POST, 'product_id', 
-                FILTER_VALIDATE_INT);
-        $product = get_product($product_id);
-        $product_code = $product['productCode'];
-
-        $image_filename = $product_code . '.png';
-        $image_dir = $doc_root . $app_path . 'images/';
-
-        if (isset($_FILES['file1'])) {
-            $source = $_FILES['file1']['tmp_name'];
-            $target = $image_dir . DIRECTORY_SEPARATOR . $image_filename;
-
-            // save uploaded file with correct filename
-            move_uploaded_file($source, $target);
-
-            // add code that creates the medium and small versions of the image
-            process_image($image_dir, $image_filename);
-
-            // display product with new image
-            include('product_view.php');
-        }
-        break;
 }
 ?>
