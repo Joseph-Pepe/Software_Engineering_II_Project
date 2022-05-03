413f263fcae4cb11539380ddae2bd75f629c8d7e
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-02 21:28:28 -0400

    Update course_database.php

--- model/course_database.php
+++ model/course_database.php
@@ -2,23 +2,26 @@
 function add_course($class_name, $instructor, $term, $day, $start_time, $end_time, $start_date, $end_date, $section) {
     global $database;
     $query = 'INSERT INTO courses (instructor, term, day, class_name, start_time, end_time, start_date, end_date, section)
-              VALUES (:category_id, :code, :name, :description, :price, :discount_percent, NOW())';
+              VALUES (:instructor, :term, :day, :class_name, :start_time, :end_time, start_date, end_date, section)';
     try {
         $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->bindValue(':code', $code);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':description', $description);
-        $statement->bindValue(':price', $price);
-        $statement->bindValue(':discount_percent', $discount_percent);
+        $statement->bindValue(':instructor', instructor);
+        $statement->bindValue(':term', $term);
+        $statement->bindValue(':day', $day);
+        $statement->bindValue(':class_name', $class_name);
+        $statement->bindValue(':start_time', $start_time);
+        $statement->bindValue(':end_time', $end_time);
+        $statement->bindValue(':start_date', $start_date);
+        $statement->bindValue(':end_date', $end_date);
+        $statement->bindValue(':section', $section);
         $statement->execute();
         $statement->closeCursor();
 
-        // Get the last product ID that was automatically generated
-        $product_id = $db->lastInsertId();
-        return $product_id;
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
+        // Get the last course ID that was automatically generated
+        $course_id = $database->lastInsertId();
+        return $course_id;
+    } catch (PDOException $exception_object) {
+        $error_message = $exception_object->getMessage();
         display_db_error($error_message);
     }
 }
