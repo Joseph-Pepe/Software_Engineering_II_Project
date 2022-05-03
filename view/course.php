c89d5103f2c54200bff31990712988eeac8ec40f
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-03 17:16:12 -0400

    Update course.php

--- view/course.php
+++ view/course.php
@@ -1,15 +1,15 @@
 <?php
     // Parse data
-    $course_number = $product['course_number'];
-    $instructor = $product['instructor'];
-    $term = $product['term'];
-    $day = $product['day'];
-    $class_name = $product['class_name'];
-    $start_time = $product['start_time'];
-    $end_time = $product['end_time'];
-    $start_date = $product['start_date'];
-    $end_date = $product['end_date'];
-    $section = $product['end_date'];
+    $course_number = $course['course_number'];
+    $instructor = $course['instructor'];
+    $term = $course['term'];
+    $days = $course['days'];
+    $class_name = $course['class_name'];
+    $start_time = $course['start_time'];
+    $end_time = $course['end_time'];
+    $start_date = $course['start_date'];
+    $end_date = $course['end_date'];
+    $section = $course['end_date'];
 
     // Get image URL and alternate text
     $image_filename = $product_code . '_m.png';
