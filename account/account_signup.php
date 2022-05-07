cabe31523219b76ebc964b9d2f1e4766768f04f1
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-07 18:20:42 -0400

    Update account_signup.php

--- account/account_signup.php
+++ account/account_signup.php
@@ -23,7 +23,7 @@ if (!isset($password_message)) { $password_message = ''; }
           <input type="submit" value="Login">
        </form>
     </header>
-    <main>
+    <main id = "forms">
        <form method = "post" id = "signup_form" class = "aligned" action = ".">
           <input name = "action" type = "hidden" value = "signup"/>
 	       
