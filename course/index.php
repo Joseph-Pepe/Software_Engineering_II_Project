e380e18ddf7f79dc1e885f244bf8e1080e027be7
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-07 21:21:18 -0400

    Update index.php

--- course/index.php
+++ course/index.php
@@ -14,42 +14,42 @@ if ($action == NULL) {
 
 switch ($action) {
     case 'list_courses':
-        $course_id = filter_input(INPUT_GET, 'course_id', FILTER_VALIDATE_INT);
-        if (empty($course_id)) {
-            $course_id = 1;
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        if (empty($course_number)) {
+            $course_number = 1;
         }
-        $current_course = get_course($course_id);
+        $current_course = get_course($course_number);
         $courses = get_all_courses($instructor);
         include('course_list.php');
         break;
     case 'view_course':
-        $course_id = filter_input(INPUT_GET, 'course_id', FILTER_VALIDATE_INT);
-        $course = get_course($course_id);
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        $course = get_course($course_number);
         include('course_view.php');
         break;
     case 'show_add_form':
-        $course_id = filter_input(INPUT_GET, 'course_id', FILTER_VALIDATE_INT);
-        if ($course_id === null) {
-            $course_id = filter_input(INPUT_POST, 'course_id', FILTER_VALIDATE_INT);
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        if ($course_number === null) {
+            $course_number = filter_input(INPUT_POST, 'course_number', FILTER_VALIDATE_INT);
         }
-        $course = get_course($course_id);
+        $course = get_course($course_number);
         include('course_add.php');
         break;
     case 'add_course':
-        $course_id = filter_input(INPUT_GET, 'course_id', FILTER_VALIDATE_INT);
-        $class_name = filter_input(INPUT_POST, 'class_name');
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        $course_name = filter_input(INPUT_POST, 'course_name');
         $term = filter_input(INPUT_POST, 'term');
         $days = filter_input(INPUT_POST, 'days', FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
         $start_end_time = filter_input(INPUT_POST, 'start_end_time');
         $section = filter_input(INPUT_POST, 'section');
         
         // Validate inputs
-        if (empty($class_name) || empty($term) || empty($section) || empty($start_end_time) || $days === NULL) {
+        if (empty($course_name) || empty($term) || empty($section) || empty($start_end_time) || $days === NULL) {
             $error = 'Invalid course data. Check all fields and try again.';
             include('../errors/error.php');
         } else {
-            $course_id = add_course($class_name, $term, $days, $start_end_time, $section);
-            $course = get_course($course_id);
+            $course_id = add_course($course_name, $term, $days, $start_end_time, $section);
+            $course = get_course($course_number);
             include('course_view.php');
         }
         break;
