5aff4bb6c6e18f8860a633955cbafcdec5078bd9
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 10:24:31 -0400

    Update create_database_test.sql

--- create_database_test.sql
+++ create_database_test.sql
@@ -41,10 +41,18 @@ INSERT INTO accounts (account_id, email_address, password, first_name, last_name
 (2, 'kk@montclair.edu', '7c6b25ee4af6db0472e5c338206a5cbae911c08b', 'Karen', 'King',  'student'), 
 (3, 'sd@montclair.edu', '8b0c689ddba0d0ebc7ab0a320635f35c0f1ae23f', 'Siperus', 'Dare', 'student'); 
 (4, 'bill@montclair.edu', 'ced6caf3d89c2a7ec8795de512ecb68fab24268d', 'Bill', 'Bob', 'student'); 
+(5, 'johnpe@montclair.edu', '11051bdbdd7cbaec855d92db2ded7cfc940b99c6', 'John', 'Pe', 'instructor');
+
+-- Step 4: Populate with three users. 
+INSERT INTO courses (course_number, instructor, term, days, course_name, start_end_time,  section) VALUES 
+(1, 'John Pe', 'fall', 'John', 'Johnson', 'instructor'), 
+(2, 'kk@montclair.edu', '7c6b25ee4af6db0472e5c338206a5cbae911c08b', 'Karen', 'King',  'student'), 
+(3, 'sd@montclair.edu', '8b0c689ddba0d0ebc7ab0a320635f35c0f1ae23f', 'Siperus', 'Dare', 'student'); 
+(4, 'bill@montclair.edu', 'ced6caf3d89c2a7ec8795de512ecb68fab24268d', 'Bill', 'Bob', 'student'); 
 (5, 'johnpe@montclair.edu', '11051bdbdd7cbaec855d92db2ded7cfc940b99c6', 'John', 'Pe', 'instructor'); 
 
 
--- Step 4: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
+-- Step 5: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
 -- Creates a user and grants them full privileges. 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON *
