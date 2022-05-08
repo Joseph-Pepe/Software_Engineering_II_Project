68cf9cf41bb7842515cfd0d9525063e1465426d1
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 12:52:11 -0400

    Update create_course.php

--- course/create_course.php
+++ course/create_course.php
@@ -7,6 +7,7 @@
      <link rel = "stylesheet" type = "text/css" href = "Cstyles/home_page_styles.css"/>
      <link rel = "stylesheet" type = "text/css" href = "Cstyles/side_bar_styles.css"/>
  </head>
+	
 
  <body id="flexParent">
     <div id="flexLeft">
