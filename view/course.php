c56030844d4de6a1fe878e62c8a13a6905666d7f
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 12:38:50 -0400

    Update course.php

--- view/course.php
+++ view/course.php
@@ -8,17 +8,10 @@
     $start_end_time = $course['start_end_time'];
     $section = $course['section'];
 ?>
-<p><b>Course Name:</b>
-   <?php echo $course_name; ?></p>
-<p><b>Course Number:</b>
-   <?php echo $course_number; ?></p>
-<p><b>Instructor:</b>
-   <?php echo $instructor; ?></p>
-<p><b>Term:</b>
-   <?php echo $term; ?></p>
-<p><b>Days:</b>
-   <?php echo $days; ?></p>
-<p><b>Start & End Time:</b>
-   <?php echo $start_end_time; ?></p>
-<p><b>Section:</b>
-   <?php echo $section; ?></p>
+<p><b>Course Name:</b><?php echo $course_name; ?></p>
+<p><b>Course Number:</b><?php echo $course_number; ?></p>
+<p><b>Instructor:</b><?php echo $instructor; ?></p>
+<p><b>Term:</b><?php echo $term; ?></p>
+<p><b>Days:</b><?php echo $days; ?></p>
+<p><b>Start & End Time:</b><?php echo $start_end_time; ?></p>
+<p><b>Section:</b><?php echo $section; ?></p>
