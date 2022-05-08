4b907b2bd37ff7967937157568903b062b53e130
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 11:07:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -126,6 +126,32 @@ switch($action){
       $course = get_course($course_number);
       include('course/course_view.php');
       break;
+   case 'show_add_form':
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        if ($course_number === null) {
+            $course_number = filter_input(INPUT_POST, 'course_number', FILTER_VALIDATE_INT);
+        }
+        $course = get_course($course_number);
+        include('course/course_add.php');
+        break;
+    case 'add_course':
+        $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
+        $course_name = filter_input(INPUT_POST, 'course_name');
+        $term = filter_input(INPUT_POST, 'term');
+        $days = filter_input(INPUT_POST, 'days', FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
+        $start_end_time = filter_input(INPUT_POST, 'start_end_time');
+        $section = filter_input(INPUT_POST, 'section');
+        
+        // Validate inputs
+        if (empty($course_name) || empty($term) || empty($section) || empty($start_end_time) || $days === NULL) {
+            $error = 'Invalid course data. Check all fields and try again.';
+            include('errors/error.php');
+        } else {
+            $course_id = add_course($course_name, $term, $days, $start_end_time, $section);
+            $course = get_course($course_number);
+            include('course_view.php');
+        }
+        break;
    case 'logout':
       unset($_SESSION['user']);
       redirect('.');
