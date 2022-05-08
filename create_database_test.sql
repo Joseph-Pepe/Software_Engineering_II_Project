057ae80fbfe2f526c8b7635e09ab12321e536267
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 10:29:34 -0400

    Update create_database_test.sql

--- create_database_test.sql
+++ create_database_test.sql
@@ -39,14 +39,14 @@ CREATE TABLE courses(
 INSERT INTO accounts (account_id, email_address, password, first_name, last_name,  account_type) VALUES 
 (1, 'jj@montclair.edu', '9788b833f5e94013b7e83a51a4792ea322020945', 'John', 'Johnson', 'instructor'), 
 (2, 'kk@montclair.edu', '7c6b25ee4af6db0472e5c338206a5cbae911c08b', 'Karen', 'King',  'student'), 
-(3, 'sd@montclair.edu', '8b0c689ddba0d0ebc7ab0a320635f35c0f1ae23f', 'Siperus', 'Dare', 'student'); 
-(4, 'bill@montclair.edu', 'ced6caf3d89c2a7ec8795de512ecb68fab24268d', 'Bill', 'Bob', 'student'); 
+(3, 'sd@montclair.edu', '8b0c689ddba0d0ebc7ab0a320635f35c0f1ae23f', 'Siperus', 'Dare', 'student'),
+(4, 'bill@montclair.edu', 'ced6caf3d89c2a7ec8795de512ecb68fab24268d', 'Bill', 'Bob', 'student'),
 (5, 'johnpe@montclair.edu', '11051bdbdd7cbaec855d92db2ded7cfc940b99c6', 'John', 'Pe', 'instructor');
 
 -- Step 4: Populate with three users. 
 INSERT INTO courses (course_number, instructor, term, days, course_name, start_end_time,  section) VALUES 
-(1, 'John Pe', 'fall (08/01/22 - 12/10/22)', '[mon, fri]', 'Software Engineering II', '01'),
-(2, 'John Pe', 'fall (08/01/22 - 12/10/22)', '[mon, fri]', 'Software Engineering I', '01'),
+(1, 'John Pe', 'fall (08/01/22 - 12/10/22)', '[mon, fri]', 'Software Engineering II', '8:40am - 10:25am', '01'),
+(2, 'John Pe', 'fall (08/01/22 - 12/10/22)', '[mon, fri]', 'Software Engineering I', '10:25am - 12:00pm', '01');
 
 -- Step 5: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
 -- Creates a user and grants them full privileges. 
