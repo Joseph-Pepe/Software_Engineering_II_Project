c009cf13fa2e5609911eaf444cab7186800308a7
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 11:47:12 -0400

    Update homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -18,7 +18,7 @@ require_once('model/course_database.php');
          <?php include 'view/side_bar.php'; ?>
       </div>
       <div id="flexRight">
-         <h2>Welcome <?php echo $user_name; ?></h2>
+         <h2>Welcome <?php echo $account_type . ': ' . $user_name; ?></h2>
 	 <hr/>
 	 <h1 class="top">My Courses</h1>
          <p>To view a course, select it (press it).</p>
