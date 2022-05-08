0ec04b9a621f0af61bbff574d88b52fbd43c1926
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 12:42:10 -0400

    Update course_view.php

--- course/course_view.php
+++ course/course_view.php
@@ -1,7 +1,25 @@
-<?php include 'view/side_bar.php'; ?>
-<main>
-    <h2>View Course</h2>
+<?php
+require_once('utility/main.php');
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <title>Homepage</title>
+      <link rel = "stylesheet" type = "text/css" href = "Cstyles/home_page_styles.css"/>
+      <link rel = "stylesheet" type = "text/css" href = "Cstyles/side_bar_styles.css"/>
+   </head>
+	
+   <body id="flexParent"> 
+      <div id="flexLeft">
+         <?php include 'view/side_bar.php'; ?>
+      </div>
+      <div id="flexRight">
+         <h2>View Course</h2>
     
-    <!-- display course -->
-    <?php include 'view/course.php'; ?>
-</main>
+         <!-- display course -->
+         <?php include 'view/course.php'; ?>
+      </div>
+   </body>
+</html>
