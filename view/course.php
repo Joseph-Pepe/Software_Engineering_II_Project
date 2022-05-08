f0bb1a9323abef5e05ab167bd8d29b233680ba32
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-08 12:37:56 -0400

    Update course.php

--- view/course.php
+++ view/course.php
@@ -8,19 +8,17 @@
     $start_end_time = $course['start_end_time'];
     $section = $course['section'];
 ?>
-<div id="right_column">
-    <p><b>Course Name:</b>
-        <?php echo $course_name; ?></p>
-    <p><b>Course Number:</b>
-        <?php echo $course_number; ?></p>
-    <p><b>Instructor:</b>
-        <?php echo $instructor; ?></p>
-    <p><b>Term:</b>
-        <?php echo $term; ?></p>
-    <p><b>Days:</b>
-        <?php echo $days; ?></p>
-    <p><b>Start & End Time:</b>
-        <?php echo $start_end_time; ?></p>
-    <p><b>Section:</b>
-        <?php echo $section; ?></p>
-</div>
+<p><b>Course Name:</b>
+   <?php echo $course_name; ?></p>
+<p><b>Course Number:</b>
+   <?php echo $course_number; ?></p>
+<p><b>Instructor:</b>
+   <?php echo $instructor; ?></p>
+<p><b>Term:</b>
+   <?php echo $term; ?></p>
+<p><b>Days:</b>
+   <?php echo $days; ?></p>
+<p><b>Start & End Time:</b>
+   <?php echo $start_end_time; ?></p>
+<p><b>Section:</b>
+   <?php echo $section; ?></p>
