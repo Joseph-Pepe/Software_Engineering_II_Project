28a8b083525537232009292a55071c81e81572a0
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 18:47:11 -0400

    Update create_database_test.sql

--- create_database_test.sql
+++ create_database_test.sql
@@ -34,13 +34,20 @@ CREATE TABLE course_roster(
    INDEX course_number (course_number)
 );
 
--- Step 3: Populate with three users. 
+-- Step 3: Populate with users. 
 INSERT INTO accounts (account_id, email_address, password, first_name, last_name,  account_type) VALUES 
 (1, 'jj@montclair.edu', '9788b833f5e94013b7e83a51a4792ea322020945', 'John', 'Johnson', 'instructor'), 
 (2, 'kk@montclair.edu', '7c6b25ee4af6db0472e5c338206a5cbae911c08b', 'Karen', 'King',  'student'), 
 (3, 'sd@montclair.edu', '8b0c689ddba0d0ebc7ab0a320635f35c0f1ae23f', 'Siperus', 'Dare', 'student'),
 (4, 'bill@montclair.edu', 'ced6caf3d89c2a7ec8795de512ecb68fab24268d', 'Bill', 'Bob', 'student'),
 (5, 'johnpe@montclair.edu', '11051bdbdd7cbaec855d92db2ded7cfc940b99c6', 'John', 'Pe', 'instructor');
+
+INSERT INTO course_roster (roster_number, course_number, student_full_name, student_email) VALUES 
+(1, 1, 'John Johnson', 'jj@montclair.edu'), 
+(2, 1, 'Karen King', 'kk@montclair.edu'), 
+(3, 1, 'Siperus Dare', 'sd@montclair.edu'),
+(4, 2, 'Bill Bob', 'bill@montclair.edu'),
+(5, 2, 'John Pe', 'johnpe@montclair.edu');
 
 -- Step 4: Create a database server user named "account_username" that can query database (main purpose is so it can be used in the creation of a PDO object).
 -- Creates a user and grants them full privileges. 
