a96afa6fa674eea4ffbe8920a71205f16b3e5a12
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 17:53:53 -0400

    Create student.php

---
+++ course_roster/student.php
@@ -0,0 +1,56 @@
+<?php include 'view/header.php'; ?>
+<?php include 'view/sidebar_admin.php'; ?>
+<main>
+    <h1>Roster</h1>
+    <p>Student: <?php echo htmlspecialchars($student_name) . ' (' . htmlspecialchars($student_email) . ')'; ?></p>
+  
+    <form action="." method="post" >
+       <input type="hidden" name="action" value="confirm_delete">
+       <input type="hidden" name="order_id" value="<?php echo $order_id; ?>">
+       <input type="submit" value="Delete Order">
+    </form>
+
+    <h2>Order Items</h2>
+    <table id="cart">
+        <tr id="cart_header">
+            <th class="left">Item</th>
+            <th class="right">List Price</th>
+            <th class="right">Savings</th>
+            <th class="right">Your Cost</th>
+            <th class="right">Quantity</th>
+            <th class="right">Line Total</th>
+        </tr>
+        <?php
+        $subtotal = 0;
+        foreach ($order_items as $item) :
+            $product_id = $item['productID'];
+            $product = get_product($product_id);
+            $item_name = $product['productName'];
+            $list_price = $item['itemPrice'];
+            $savings = $item['discountAmount'];
+            $your_cost = $list_price - $savings;
+            $quantity = $item['quantity'];
+            $line_total = $your_cost * $quantity;
+            $subtotal += $line_total;
+            ?>
+            <tr>
+                <td><?php echo htmlspecialchars($item_name); ?></td>
+                <td class="right">
+                    <?php echo sprintf('$%.2f', $list_price); ?>
+                </td>
+                <td class="right">
+                    <?php echo sprintf('$%.2f', $savings); ?>
+                </td>
+                <td class="right">
+                    <?php echo sprintf('$%.2f', $your_cost); ?>
+                </td>
+                <td class="right">
+                    <?php echo $quantity; ?>
+                </td>
+                <td class="right">
+                    <?php echo sprintf('$%.2f', $line_total); ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+</table>
+</main>\
