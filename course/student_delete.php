d3c1f93167427a6f51c2ac7086a4b362aceebdec
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 19:50:47 -0400

    Update student_delete.php

--- course/student_delete.php
+++ course/student_delete.php
@@ -1,20 +1,13 @@
-<?php include 'view/header.php'; ?>
-<?php include 'view/sidebar_admin.php'; ?>
-
 <main class="nofloat">
     <h1>Delete Account</h1>
-    <p>Are you sure you want to delete the account for
-       <?php echo htmlspecialchars($last_name) . ', ' . 
-                  htmlspecialchars($first_name) .
-                  ' (' . htmlspecialchars($email) . ')'; ?>?</p>
+    <p>Are you sure you want to delete this student?</p>
     <form action="." method="post">
         <input type="hidden" name="action" value="delete">
-        <input type="hidden" name="admin_id"
-               value="<?php echo $admin_id; ?>">
-        <input type="submit" value="Delete Account">
+        <input type="hidden" name="roster_number"
+               value="<?php echo $roster_number; ?>">
+        <input type="submit" value="Delete Student">
     </form>
     <form action="." method="post">
         <input type="submit" value="Cancel">
     </form>
 </main>
-<?php include 'view/footer.php'; ?>
