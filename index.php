8131c29123496255a936aa5360327aff4df56a30
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 22:01:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -132,10 +132,8 @@ switch($action){
     case 'delete':
         $roster_number = filter_input(INPUT_POST, 'roster_number', FILTER_VALIDATE_INT);
         $course_number = filter_input(INPUT_POST, 'course_number', FILTER_VALIDATE_INT);
-        $course = get_course($course_number);
-        $roster = get_course_roster($course_number);
         delete_student($roster_number);
-        include('course/course_view.php');
+        redirect($app_path);
         break;
    case 'show_add_form':
         $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
