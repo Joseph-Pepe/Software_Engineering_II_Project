4423c0ea8c24513232e03a199aa61405471b70e7
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 20:38:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -137,11 +137,11 @@ switch($action){
     case 'delete':
         $user_name = $_SESSION['user']['first_name'] . ' ' . $_SESSION['user']['last_name'];
         $course_number = filter_input(INPUT_POST, 'course_number', FILTER_VALIDATE_INT);
+        $roster_number = filter_input(INPUT_POST, 'roster_number', FILTER_VALIDATE_INT);
         $course = get_course($course_number);
         $roster = get_course_roster($course_number);
-        $roster_number = filter_input(INPUT_POST, 'roster_number', FILTER_VALIDATE_INT);
         delete_student($course_number, $roster_number);
-        include('course/course_view.php');
+        redirect($app_path . 'course/course_view');
         break;
    case 'show_add_form':
         $course_number = filter_input(INPUT_GET, 'course_number', FILTER_VALIDATE_INT);
