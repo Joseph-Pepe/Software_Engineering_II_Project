0e6afed7b5448b330c7ab03584d5b0258bc0bb35
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-05-09 23:25:45 -0400

    Update course_database.php

--- model/course_database.php
+++ model/course_database.php
@@ -23,23 +23,19 @@ function add_course($course_name, $instructor, $term, $days, $start_end_time, $s
     }
 }
 
-function add_student_to_course_roster($course_name, $instructor, $term, $days, $start_end_time, $section) {
+function add_student_to_course_roster($roster_number, $course_number, $student_full_name, $student_email) {
     global $database;
-    $query = 'INSERT INTO courses (instructor, term, days, course_name, start_end_time, section)
-              VALUES (:instructor, :term, :days, :course_name, :start_end_time, :section)';
+    $query = 'INSERT INTO course_roster (roster_number, course_number, student_full_name, student_email)
+              VALUES (:roster_number, :course_number, :student_full_name, :student_email)';
     try {
         $statement = $database->prepare($query);
-        $statement->bindValue(':instructor', $instructor);
-        $statement->bindValue(':term', $term);
-        $statement->bindValue(':days', $days);
-        $statement->bindValue(':course_name', $course_name);
-        $statement->bindValue(':start_end_time', $start_end_time);
-        $statement->bindValue(':section', $section);
+        $statement->bindValue(':roster_number', $roster_number);
+        $statement->bindValue(':course_number', $course_number);
+        $statement->bindValue(':student_full_name', $student_full_name);
+        $statement->bindValue(':student_email', $student_email);
         $statement->execute();
         $statement->closeCursor();
-
-        // Get the last course ID that was automatically generated
-        $course_id = $database->lastInsertId();
+        $course_roster_id = $database->lastInsertId();
         return $course_id;
     } catch (PDOException $exception_object) {
         $error_message = $exception_object->getMessage();
